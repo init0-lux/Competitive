a2c5ad9d46b0b6d1df8cb287b6f139e204d16311
init0-lux <init0.lux@protonmail.com>
2021-12-09 02:56:58 +0530

    Beginner overflow

---
+++ Beginner/overflow.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// int -> 10^9
+	// long -> 10^12
+	// long long -> 10^18
+	
+	cout << LONG_LONG_MAX << endl;
+	cout << SHRT_MAX << endl;
+	return 0;
+}
