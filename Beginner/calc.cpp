9a843112ab9e369c6dd06003b90bd074dcbcd957
init0-lux <init0.lux@protonmail.com>
2021-12-09 02:56:58 +0530

    Beginner calc

---
+++ Beginner/calc.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Precedence:
+// Double
+// Float
+// Long long
+// Long
+// int
+// char
+//
+// 3/2 = int
+// 3/2.0 = double
+
+int main() {
+	return 0;
+}
