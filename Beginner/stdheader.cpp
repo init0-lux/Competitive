d8e5a60d02b8357e421a11c8833a26c16417aea5
init0-lux <init0.lux@protonmail.com>
2021-12-09 02:56:58 +0530

    Beginner stdheader

---
+++ Beginner/stdheader.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+// for competitive programming, this is all you need
+
+using namespace std;
+
+int main() {
+	cout << "Hello, World!" << endl;
+	cout << sqrt(16) << endl;
+	
+	bool b=true;
+	bool d=false;
+
+	cout << b << " " << d << endl;
+
+	return 0;
+}
