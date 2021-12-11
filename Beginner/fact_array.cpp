0f5fd4dd5f168e4d9f7c0a8526e1b92286e07ce7
init0-lux <init0.lux@protonmail.com>
2021-12-12 02:56:59 +0530

    Beginner fact_array

---
+++ Beginner/fact_array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t > 0) {
+		long long arr[10000];
+		int j = 10000;
+		arr[0] = 1; // initialize
+
+
+		--t;
+	}
+	
+	return 0;
+}
