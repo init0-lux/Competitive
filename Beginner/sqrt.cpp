7636a9fb3cee529033e58b704966617a268f6f86
init0-lux <init0.lux@protonmail.com>
2021-12-12 02:56:59 +0530

    Beginner sqrt

---
+++ Beginner/sqrt.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while(T>0){
+		int N;
+
+		cin >> N;
+
+		cout << (int)floor(sqrt(N)) << endl;
+
+		--T;
+	}
+	
+	return 0;
+}
