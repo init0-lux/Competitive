fac658c1bafa19a5c12d9c4b98ed16b0898adea0
init0-lux <init0.lux@protonmail.com>
2021-12-12 02:56:59 +0530

    Beginner remainder

---
+++ Beginner/remainder.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while(T>0){
+		int A, B;
+		cin >> A >> B;
+		cout << A % B << "\n";
+
+		--T;
+	}
+	
+	return 0;
+}
