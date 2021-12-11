49bd57f77cfa9d20f94938249bcfe6e622873d57
init0-lux <init0.lux@protonmail.com>
2021-12-12 02:56:59 +0530

    Beginner do

---
+++ Beginner/do.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long factorial(int num) {
+	long long ret=1;
+
+	for(int i = 1; i <= num; i++ )
+		ret *= i;
+
+	return ret;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t>0){
+		int num;
+		cin >> num;
+
+		cout << factorial(num) << endl;
+
+		--t;
+	}
+	
+	return 0;
+}
+
