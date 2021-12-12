cb900fb9211814301375e84f12e2950cf6dd568a
init0-lux <init0.lux@protonmail.com>
2021-12-13 02:56:59 +0530

    Beginner valid

---
+++ Beginner/valid.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while(T--){
+		int A, B, C;
+		cin >> A >> B >> C;
+
+		if( A + B + C == 180)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+	
+	return 0;
+}
