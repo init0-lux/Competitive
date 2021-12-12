a64acbcb1b3489ee50b81770f124dee89170e644
init0-lux <init0.lux@protonmail.com>
2021-12-13 02:56:59 +0530

    Beginner remiss

---
+++ Beginner/remiss.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while(T--){
+		int A, B;
+		cin >> A >> B;
+		if(A>B)
+			cout << A << " " << A+B << endl;
+		else
+			cout << B << " " << A+B << endl;
+	}
+	
+	return 0;
+}
