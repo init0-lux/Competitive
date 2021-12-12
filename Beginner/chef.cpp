f958e14cfbd1a1c9ceed8eaf451a33c9c742d0a5
init0-lux <init0.lux@protonmail.com>
2021-12-13 02:56:59 +0530

    Beginner chef

---
+++ Beginner/chef.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+		int N;
+		cin >> N;
+
+		if(N < 10)
+			cout << "Thanks for helping Chef!" << endl;
+		else
+			cout << -1 << endl;
+	}
+	
+	return 0;
+}
