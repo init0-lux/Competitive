43d4ae78261debfcbd0aab93298e66e84147e41d
init0-lux <init0.lux@protonmail.com>
2021-12-25 02:57:00 +0530

    HackerRank data

---
+++ HackerRank/data.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	ll a, b;
+	char c;
+	float d; double e;
+
+	cin >> a >> b >> c >> d >> e;
+	cout << a << endl << b << endl << c << endl;
+	cout << fixed << d << endl;
+	cout << fixed << e << endl;
+
+	return 0;
+}
