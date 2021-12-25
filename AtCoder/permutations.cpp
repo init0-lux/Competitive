a08513f1823c06f3fbf37005452fc8d5e83dd2d5
init0-lux <init0.lux@protonmail.com>
2021-12-26 02:57:00 +0530

    AtCoder permutations

---
+++ AtCoder/permutations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	int n; cin >> n;
+	int R[n], C[n];
+
+	for(int i = 0; i < n; i++) cin >> R[i];
+	for(int i = 0; i < n; i++) cin >> C[i];
+
+	int q; cin >> q;
+	int r[q], c[q];
+
+	for(int i = 0; i < q; i++) cin >> r[i];
+	for(int i = 0; i < q; i++) cin >> c[i];
+
+	return 0;
+}
