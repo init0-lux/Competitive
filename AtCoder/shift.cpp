65ef21794831d95a5537831616bbbd8e5626d659
init0-lux <init0.lux@protonmail.com>
2021-12-26 02:57:00 +0530

    AtCoder shift

---
+++ AtCoder/shift.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	int n; cin >> n;
+	int p[n];
+
+	for(int i = 0; i < n; i++) cin >> p[i];
+
+	if (not is_sorted( p, p+n )){
+			
+	}
+
+	else cout << 0 << endl;
+
+	return 0;
+}
