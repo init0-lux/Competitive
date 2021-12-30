cfd0ee99721fc3c49d24970c2ad9d4a0d24aa273
init0-lux <init0.lux@protonmail.com>
2021-12-31 02:57:00 +0530

    Codeforces capital

---
+++ Codeforces/capital.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	string a;
+	cin >> a;
+
+	a[0] = toupper(a[0]);
+
+	cout << a << endl;
+	return 0;
+}
