12afd90f4cff31c10cd0d9db51be3920ea3f3a54
init0-lux <init0.lux@protonmail.com>
2021-12-31 02:57:00 +0530

    Codeforces domino

---
+++ Codeforces/domino.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	int M, N;
+	cin >> M >> N;
+	int A[M][N];
+
+	cout << floor( N * M * 0.5 ) << endl;
+
+	return 0;
+}
