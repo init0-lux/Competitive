6f6888d18472545ac49797395b444fd16f213a9e
init0-lux <init0.lux@protonmail.com>
2022-01-01 02:57:01 +0530

    Codeforces elephant

---
+++ Codeforces/elephant.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	int x; cin >> x;
+
+	cout << ceil( float(x) / 5.0 ) << endl;
+
+	return 0;
+}
