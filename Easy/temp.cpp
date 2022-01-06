ffc045ecb14701ffb9f9167048d3c67331aa5f31
init0-lux <init0.lux@protonmail.com>
2022-01-07 02:57:02 +0530

    Easy temp

---
+++ Easy/temp.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main() {
+	fast();
+
+	return 0;
+}
