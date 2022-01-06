e43a899ea33b64d1b48ad5c542ac17fc743edb44
init0-lux <init0.lux@protonmail.com>
2022-01-07 02:57:02 +0530

    Codeforces rooks

---
+++ Codeforces/rooks.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--){
+		int n, k;
+		cin >> n >> k;
+
+
+	}
+
+	return 0;
+}
