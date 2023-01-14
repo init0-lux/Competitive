494d419bbd16b8257364e6e1a1ec0a286be4d39f
init0-lux <init0.lux@protonmail.com>
2023-01-15 02:57:02 +0530

    CodeChef-ZCO weightage

---
+++ CodeChef-ZCO/weightage.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+	fast();
+
+	int N, P;
+	cin >> N >> P;
+
+	if (N == 1){
+		cout << 1 << endl << 1;
+	}
+
+	for (int i = 0; i < N; i++){
+
+	}
+	
+
+	return 0;
+}
