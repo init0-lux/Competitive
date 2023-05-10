fdbaec96eb5ac67e4d50b7e3d0fb57969ea362e1
init0-lux <init0.lux@protonmail.com>
2023-05-11 02:57:02 +0530

    HackerRank sprial

---
+++ HackerRank/sprial.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typdef long long ll;
+#define endl '\n';
+
+int main(){
+	int n; cin >> n;
+	int A[n][n];
+
+/*
+ * 11 12 13 14
+ * 21 22 23 24
+ * 31 32 33 34
+ * 41 42 43 44
+ *
+ * 1(1,2,3,4) -> (1,2,3,4)4 -> (4,3,2,1)4 -> (4,3,2)1 -> (2,3)2 -> 3(3,2)
+ */
+}
